d3d394689b93fbc39795764c030dd58640044b6c
Nicholas Kohut <nicholaskohut@nicholass-air-2.lan>
2023-03-24 22:27:15 -0700

    added plot function

--- IP10k_CI.py
+++ IP10k_CI.py
@@ -1,7 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.stats import gamma
 
-def IPM_CIs(num_events, num_miles, ci_width):
+def IP10k_CIs(num_events, num_miles, ci_width):
     alpha = 1 - ci_width
     lower_ci = (gamma.ppf(alpha/2, num_events, 1) - 1) / num_miles
     upper_ci = (gamma.ppf(1-alpha/2, num_events+1, 1) - 1) / num_miles
@@ -11,5 +12,13 @@ def IPM_CIs(num_events, num_miles, ci_width):
 
     return [IP10k_lower_ci, IP10k_upper_ci]
 
+def plot_CIs(num_events, mileage_sweep, ci_width):
+    IP10k_ci_list = []
+    for miles in mileage_sweep:
+        IP10k_ci = IP10k_CIs(num_events, miles, ci_width)
+        IP10k_ci_list.append(IP10k_ci)
 
-print(IPM_CIs(1, 15000, 0.95))
+    plt.plot(mileage_sweep, IP10k_ci_list)
+    plt.show()
+
+plot_CIs(1, np.linspace(3000, 15000, num = 30), 0.95)
