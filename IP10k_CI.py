a8ed61a9092cf127e47f7729fa1b021fe510a437
Nicholas Kohut <nicholaskohut@nicholass-air-2.lan>
2023-03-24 22:02:19 -0700

    Fixing the formula up for CIs

--- IP10k_CI.py
+++ IP10k_CI.py
@@ -3,13 +3,13 @@ from scipy.stats import gamma
 
 def IPM_CIs(num_events, num_miles, ci_width):
     alpha = 1 - ci_width
-    lower_ci = gamma.ppf(alpha/2, num_events, 1) / num_miles
-    upper_ci = gamma.ppf(1-alpha/2, num_events+1, 1) / num_miles
+    lower_ci = (gamma.ppf(alpha/2, num_events, 1) - 1) / num_miles
+    upper_ci = (gamma.ppf(1-alpha/2, num_events+1, 1) - 1) / num_miles
 
-    IP10k_lower_ci = lower_ci * 10000 - 1
-    IP10k_upper_ci = upper_ci * 10000 - 1
+    IP10k_lower_ci = lower_ci * 10000
+    IP10k_upper_ci = upper_ci * 10000
 
     return [IP10k_lower_ci, IP10k_upper_ci]
 
 
-print(IPM_CIs(1, 10000, 0.95))
+print(IPM_CIs(1, 15000, 0.95))
